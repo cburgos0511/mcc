d4323990c1aac1154e7302d498b702701d2eda1e
Cruz Burgos <cburgos@nodecentric.com>
2020-09-07 20:17:05 -0500

    :sparkles: Added contact page with map

---
+++ src/components/Contacts/Map.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import s from './map.scss'
+import { graphql, useStaticQuery } from 'gatsby'
+import GoogleMapReact from 'google-map-react'
+
+const Marker = ({ text, $hover }) => {
+    console.log($hover)
+
+    const style = $hover ? s.markerScale : s.marker
+
+    return <div className={style}></div>
+}
+
+const Map = () => {
+    // const { allLocationData } = useStaticQuery(
+    //     graphql`
+    //         query LocationQuery {
+    //             allLocationData {
+    //                 edges {
+    //                     node {
+    //                         results {
+    //                             geometry {
+    //                                 location_type
+    //                                 location {
+    //                                     lat
+    //                                     lng
+    //                                 }
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     `
+    // )
+
+    return (
+        <div>
+            <p>hello@millardcommunity.church</p>
+            <p className={s.address}>12656 Weir St, Omaha, NE 68137</p>
+
+            <div className={s.map}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{
+                        key: 'AIzaSyAQTy5W2_46KgjaqDbR_MJHyLSx0nR-s70',
+                    }}
+                    defaultCenter={{ lat: 41.2064881, lng: -96.1117639 }}
+                    defaultZoom={16}
+                    hoverDistance={75}
+                >
+                    <Marker text="MCC" lat={41.2064881} lng={-96.1117639} />
+                </GoogleMapReact>
+            </div>
+        </div>
+    )
+}
+
+export default Map
---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Header from '../components/Contacts/Header'
+import Map from '../components/Contacts/Map'
+import ContactForm from '../components/Contacts/ContactForm'
+import s from '../components/Contacts/contact.scss'
+
+const contact = () => {
+    
+    return (
+        <>
+            <Header />
+            <div className={s.contactContainer}>
+                <Map />
+                <ContactForm />
+            </div>
+        </>
+    )
+}
+
+export default contact
